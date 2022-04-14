8a9c8afcdc6e091a4473b15489a018c807cd0c0e
tako2039 <takeo2039@gmail.com>
2022-04-14 13:32:58 -0500

    start project

---
+++ Main.java
@@ -0,0 +1,15 @@
+/*********************************
+ * Your Name: Takeo Shimazu
+ * Class and Section: Sp22 CIS D036A 01Y, 64Z Java Programming
+ * Assignment Number: 1 Part 1
+ * Due Date: Apr 20, 2022
+ * Date Submitted: Apr 13, 2022
+ *******************************/
+package com.takeo;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
