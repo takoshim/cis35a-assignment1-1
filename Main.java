922e7c269c70b8e23a5883c60f9b08a09b1b6a8d
tako2039 <takeo2039@gmail.com>
2022-04-14 14:02:06 -0500

    add input and output

--- Main.java
+++ Main.java
@@ -7,9 +7,34 @@
  *******************************/
 package com.takeo;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-	// write your code here
+        System.out.print("Loan Amount: ");
+        double loanAmount = scanner.nextDouble();
+
+        System.out.print("Number of Years: ");
+        int numberOfYears = scanner.nextInt();
+
+        System.out.print("Annual Interest Rate: ");
+        double annualInterestRate = scanner.nextDouble() / 100;
+        double monthlyInterestRate = annualInterestRate / 12;
+
+        System.out.println("\nMonthly Payment: " + 865.26);
+        System.out.println("Total Payment: " + 10383.21 + "\n");
+
+        int timesOfPayments = 12 * numberOfYears;
+        double monthlyInterest = 100.00;
+        double monthlyPrincipal = 100.00;
+        double balance = 10;
+
+        System.out.printf("%-13s %-13s %-13s %-13s\n", "Payment#", "Interest", "Principal", "Balance");
+        for (int i=1; i<=timesOfPayments; i++) {
+            System.out.printf("%-13d %-13.2f %-13.2f %-13.2f\n", i, monthlyInterest, monthlyPrincipal, balance);
+        }
     }
 }
